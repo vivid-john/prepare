1a580ecc003218da7074edc8dd9a55d10b62a2b7
vivid-john <johnjobin@gmail.com>
2020-02-22 10:33:00 +0530

    Verify connectivity

---
+++ starting/src/starting/Entry.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package starting;
+
+/**
+ * @author Jobin John
+ *
+ */
+public class Entry {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
